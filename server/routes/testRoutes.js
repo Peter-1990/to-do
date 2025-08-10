61b4461fce9637c336be1b2baf1be732dfc8bc23
Peter-1990 <harip3279@gmail.com>
2025-08-10 11:16:59 +0530

    changes

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const { testingController } = require("../controllers/testController.js");
 
 
+
 const router = express.Router();
 
 router.get("/", testingController);
 
 
-module.exports = router
+module.exports = router;
